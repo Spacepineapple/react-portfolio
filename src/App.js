f01e22c5c2972884819797f4f1ab3a2c8661a39a
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-03 18:47:22 +0000

    Remove all src content and initialise App.js and index.js files and components folder with pages for contact and project pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavTabs from './components/NavTabs';
+import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
+import ProjectGallery from './components/pages/ProjectGallery';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <NavTabs />
+        <Routes basename="react-portfolio">
+          <Route path="/" element={<Home />} />
+          <Route path="projectGallery" element={<ProjectGallery />} />
+          <Route path="contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
