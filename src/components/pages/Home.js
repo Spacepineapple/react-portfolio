30707c5c033158cacde09d058bc36fa6b4a1c3f5
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-03 19:44:33 +0000

    Add portrait image to src folder, import and add to Home component and add welcome text to Home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,24 @@
 import React from "react";
+import portrait from "../../images/NealPhoto.jpg";
 
 function Home() {
     return (
         <main>
             <section>
-                <h1>Welcome</h1>
-                <p>Welcome to my personal website and portfolio.</p>
-            </section>
+                <div class="content-div" id="profile-content">
+                        <img src={portrait} class="portrait"/>
+                        <p>Hello and welcome to my personal portfolio page! My name
+                        is Neal Reeves and I'm an aspiring web developer currently based in
+                        London, England. I have a background in academic research
+                        focused on the design and implementation of large-scale
+                        volunteer crowdsourcing projects that facilitate and support
+                        scientific research (<emph>citizen science</emph>). On
+                        this page, you can find a description of my skills, links to
+                        previous programming projects I have completed and links to
+                        my relevant social media pages if you wish to contact
+                        me.</p>
+                </div>
+                </section>
         </main>
     );
 }
