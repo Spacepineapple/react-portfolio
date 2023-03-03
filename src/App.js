7523c393666cf33fd529dbdd97eeba1b2a8f06b7
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-03 20:23:36 +0000

    Initialise Footer component and style.css file and import style.css into App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Home from './components/pages/Home';
-import Contact from './components/pages/Contact';
-import ProjectGallery from './components/pages/ProjectGallery';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/pages/Home";
+import Contact from "./components/pages/Contact";
+import ProjectGallery from "./components/pages/ProjectGallery";
+import "./style.css";
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
           <Route path="projectGallery" element={<ProjectGallery />} />
           <Route path="contact" element={<Contact />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <h3>&copy; Neal Reeves, 2022</h3>
+        </footer>
+    );
+}
+
+export default Footer;
