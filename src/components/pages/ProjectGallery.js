d243e47de72f68b745f63b36999607afe5cab414
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-04 17:36:27 +0000

    Add introductory text and portfolio link to portfolio gallery page

--- src/components/pages/ProjectGallery.js
+++ src/components/pages/ProjectGallery.js
@@ -8,6 +8,7 @@ function ProjectGallery() {
         <main>
             <section>
                 <h1>Project Gallery</h1>
+                <p>Below you can see a selection of programming projects I have completed. Click to see more details about each project including a project description, a link to the GitHub repository for that project and where relevant a link to the deployed project. In addition to the projects below, I have a number of other projects which can be seen on GitHub at <a href="https://github.com/Spacepineapple/">github.com/Spacepineapple</a>.</p>
                 <div className="projects">
                     {projects.map(project => <PortfolioProject key={project.id} props={project}/>)}
                 </div>
