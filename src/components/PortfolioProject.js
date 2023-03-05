54f4ff8af064b9a9f88536296821ce3a92fad114
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-05 12:12:17 +0000

    Update file paths for static images in projects.json and component srclocations

--- src/components/PortfolioProject.js
+++ src/components/PortfolioProject.js
@@ -4,10 +4,13 @@ function PortfolioProject({props}) {
     return (
         <div className="portfolio-project">
             <h3>{props.name}</h3>
-            <img className="project-image" src={"/react-portfolio" + props.image}/>
+            <img className="project-image" src={props.image}/>
             <p>{props.technologies}</p>
         </div>
     );
 }
 
 export default PortfolioProject;
+
+//               
+
--- src/components/pages/ProjectPage.js
+++ src/components/pages/ProjectPage.js
@@ -12,17 +12,20 @@ function ProjectPage() {
                 <p></p>
                 <h1>{project.name}</h1>
                 <div className="project-page">
-                    <img src={project.image}/>
                     <div className="project-info">
                         <p>{project.description}</p>
                         <p>{project.technologies}</p>
-                        <p><a href={project.deployed}/></p>
-                        <p><a href={project.repository}/></p>
+                        <img src={project.image}/>
+                        <p><a href={project.deployed}/>{project.deployed}</p>
+                        <p><a href={project.repository}/>{project.repository}</p>
                     </div>
                 </div>
             </section>
         </main>
     );
 }
+
+//                    
+
 
 export default ProjectPage;
