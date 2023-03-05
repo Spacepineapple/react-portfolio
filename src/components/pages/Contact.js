40161f856e9456db2901c91279ec3edf38aebd44
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-05 15:31:37 +0000

    Move contact form to bottom of contact page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,9 +8,9 @@ function Contact() {
                 <h1>Contact</h1>
                 <p>You can contact me via:</p>
                     <ul>
-                        <li key="linkedIn">LinkedIn: </li>
-                        <li key="gitHub">GitHub: </li>
-                        <li key="email">Email: </li>
+                        <li key="linkedIn">LinkedIn: <a href="https://www.linkedin.com/in/neal-reeves-55a27774/">https://www.linkedin.com/in/neal-reeves-55a27774/</a></li>
+                        <li key="gitHub">GitHub: <a href="https://github.com/Spacepineapple/">https://github.com/Spacepineapple/</a></li>
+                        <li key="email">Email: <a href="mailto:neal.t.reeves@gmail.com">neal.t.reeves@gmail.com</a></li>
                     </ul>
                 <p>Or add a comment using the form below:</p>
                 <CommentForm/>
