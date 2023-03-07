765ce34757e4347ea7fa83316661ded13d275158
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-07 20:51:32 +0000

    Add icon files to public images folder, add icons to footer and add hrefs to footer icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,12 @@ function Footer() {
     return (
         <footer>
             <h3>&copy; Neal Reeves, 2023</h3>
+            <div class="icon-div">
+                <a href="mailto:neal.t.reeves@gmail.com"><img class="icon" src="/react-portfolio/images/email.png"/></a>
+                <a href="https://github.com/Spacepineapple/"><img class="icon" src="/react-portfolio/images/github-icon.png"/></a>
+                <a href="https://www.linkedin.com/in/neal-reeves-55a27774/"><img class="icon" src="/react-portfolio/images/linkedin-icon.png"/></a>
+                <a href="https://scholar.google.co.uk/citations?user=XFLPi5IAAAAJ&hl=en"><img class="icon" src="/react-portfolio/images/scholar-icon.png"/></a>
+            </div>
         </footer>
     );
 }
