e946899c9adc563d8208087738f7eae561e3b935
Spacepineapple <neal.t.reeves@gmail.com>
2023-03-08 10:31:53 +0000

    Initialise json files with education, experience, hobbies and projects for CV

--- src/components/pages/CV.js
+++ src/components/pages/CV.js
@@ -2,7 +2,13 @@ import React from "react";
 
 
 function CV() {
-    return "test";
+    return (
+        <main>
+            <section>
+                <p>Below you can see my CV including a summary of my experience, education and skills to date. To access a downloadable copy, <a href="/Neal_Reeves_CV.pdf">click here</a> or feel free to visit my LinkedIn profile <a href="https://www.linkedin.com/in/neal-reeves-55a27774/">here</a>. To find out more about my projects, visit my portfolio using the tab above or find me on GitHub <a href="https://github.com/Spacepineapple/">here</a>.</p>
+            </section>
+        </main>
+    );
 }
 
 export default CV;
